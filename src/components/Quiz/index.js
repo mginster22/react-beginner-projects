bd961a97a4a04f12653e51e064736600623ffb8a
Vlad Korochanskiy <mginster234942@gmail.com>
2022-09-04 13:17:35 +0300

    sadsadas

---
+++ src/components/Quiz/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "./Quiz.module.scss";
+
+const questions = [
+  {
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    correct: 0,
+  },
+  {
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
+    correct: 1,
+  },
+  {
+    title: "Что такое JSX?",
+    variants: [
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    correct: 2,
+  },
+];
+
+
+const Game=()=>{
+
+  return(
+    <>
+    </>
+  )
+}
+
+const Quiz = () => {
+  return (
+   <section className={styles.wrapper}>
+   </section>
+  );
+};
+
+export default Quiz;
